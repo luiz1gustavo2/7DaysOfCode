dda0d6aa4c29dbb3e1592c0cc26134a91a4a79e3
Luiz Gustavo <luiz1gustavo2@hotmail.com>
2022-03-23 17:49:25 -0300

    Desafio resolvido Day2

---
+++ Day2/Day2.js
@@ -0,0 +1,27 @@
+var link = 'https://www.alura.com.br/cursos-online-programacao';
+const nome = prompt("Qual seu nome?");
+const idade = prompt("Quantos anos você tem?");
+const linguagem = prompt("Qual linguagem estuda?");
+const tempo = prompt(`Você estuda ${linguagem} há quantos anos?`);
+const msg = `<h1>${nome}, tem ${idade} anos e estuda ${linguagem} há ${tempo} anos.</h1></br>`;
+document.writeln(msg);
+
+resposta = prompt(`Você gosta de estudar ${linguagem}? Responda com 1 para SIM ou 2 para NÃO.`);
+
+if (resposta == 1) {
+  const msg2 = `<p>Muito bem ${nome}, continue se esforçando estudando ${linguagem} e você terá um futuro brilhante.</p>`;
+  document.writeln(msg2);
+  console.log(msg2)
+} else {
+  resposta2 = prompt("Ahh que pena... Já tentou aprender outras linguagens? Responda com 1 para SIM ou 2 para NÃO.");
+}
+
+if(resposta2 == 1) {
+  const msg3 = `<p>Infelizmente você não gosta de estudar ${linguagem}, porém está tentando achar uma linguagem que goste, continue assim, <a href="${link}">aqui uma sugestão de cursos de linguagens que vc pode se interessar.</a> Boa sorte!</p>`;
+  document.writeln(msg3);
+  console.log(msg3)
+} else {
+  const msg4 = `<p>Já que você não gosta de estudar ${linguagem}, <a href="${link}">fica aqui uma sugestão de cursos de linguagens que vc pode gostar. </a>Não desista!</p>`;
+  document.writeln(msg4);
+  console.log(msg4)
+}
