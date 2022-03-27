2454f93e2005cd018d3b161e090c35cfec77d5b0
Luiz Gustavo <luiz1gustavo2@hotmail.com>
2022-03-26 22:23:33 -0300

    Desafio Day5

---
+++ Day5/Day5.js
@@ -0,0 +1,13 @@
+let frutas = [];
+let sobremesas = [];
+let vegetais = [];
+let legumes = [];
+let congelados = [];
+
+function comprar(comida) {
+  comida = prompt("Qual comida você deseja adicionar na sua lista?");
+  tipoComida = prompt("Em que tipo de comida essa comida se enquadra?");
+  if (tipoComida == fruta) {
+    frutas.push(comida);
+  }
+}
