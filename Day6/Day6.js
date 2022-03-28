054eb03abcdf82af04b7b9af5baeef36b7eddcc1
Luiz Gustavo <luiz1gustavo2@hotmail.com>
2022-03-28 18:57:37 -0300

    Desafio resolvido Day6

--- Day6/Day6.js
+++ Day6/Day6.js
@@ -3,30 +3,48 @@ const fruta = [];
 const sobremesa = [];
 const legume = [];
 const congelado = [];
-  
-let pergunta = prompt("Você deseja adicionar algum alimento na sua lista, sim ou não?");
 
-while(pergunta === "sim") {
-  var alimento = prompt("Qual alimento?")
-  var tipoComida = prompt("Em que tipo de alimento esse alimento se enquadra; fruta, sobremesa, congelado ou legume?");
-  
-  if (tipoComida == "fruta") {
-  fruta.push(alimento);
-  } else if (tipoComida == "sobremesa") {
-  sobremesa.push(alimento);
-  } else if (tipoComida == "legume") {
-  legume.push(alimento);
-  } else if (tipoComida == "congelado") {
-  congelado.push(alimento);
+function comprar() {
+  let pergunta = prompt("Você quer adicionar ou remover um item?");
+
+  if (pergunta === "adicionar") {
+    adicionarItem();
+  } else if (pergunta === "remover") {
+    removerItem();
   } else {
-  alert("Eu não conheço esse tipo de comida.")
+    alert("Obrigado pela preferência");
   }
-
-  pergunta = prompt("Você deseja adicionar outro alimento, sim ou não?");
-
 }
 
-if (pergunta === "não") {
+function adicionarItem() {
+
+    while(true) {
+      var alimento = prompt("Qual alimento?")
+      var tipoComida = prompt("Em que tipo de alimento esse alimento se enquadra; fruta, sobremesa, congelado ou legume?");
+      
+      if (tipoComida == "fruta") {
+      fruta.push(alimento);
+      } else if (tipoComida == "sobremesa") {
+      sobremesa.push(alimento);
+      } else if (tipoComida == "legume") {
+      legume.push(alimento);
+      } else if (tipoComida == "congelado") {
+      congelado.push(alimento);
+      } else {
+      alert("Eu não conheço esse tipo de comida.")
+      }      
+    
+    pergunta = prompt("Você deseja adicionar outro alimento, sim ou não?");
+
+    if (pergunta === "não") {
+      alert("Obrigado volte sempre!");
+      mostraListaNaTela();
+      break;
+    }
+  }
+}
+
+function mostraListaNaTela() {
   lista.innerHTML = `<h1>Lista de Compras</h1>
   <br>
   <h2>Frutas:</h2> 
@@ -39,11 +57,35 @@ if (pergunta === "não") {
   <br> 
   <h2>Legumes:</h2> 
   <p>${legume}</p>`;
-
 }
 
-function remover() {
-  
-}
+function removerItem() {
+  while (true) {
+    let tipoComida = prompt(
+      "Qual o tipo de alimento que você deseja remover, fruta, sobremesa, congelado ou legume?)");
 
-console.log(alimento ,fruta);
+    let alimento = prompt("E qual alimento você deseja remover?");
+
+    if (tipoComida === "fruta") {
+      fruta.splice(fruta.indexOf(alimento), 1);
+    }
+    if (tipoComida === "sobremesa") {
+      sobremesa.splice(sobremesa.indexOf(alimento), 1);
+    }
+    if (tipoComida === "congelado") {
+      congelado.splice(congelado.indexOf(alimento), 1);
+    }
+    if (tipoComida === "legume") {
+      legume.splice(legume.indexOf(alimento), 1);
+    }
+
+    alert("O alimento será removido.");
+
+    let pergunta = prompt("Deseja remover outro item, sim ou não?");
+    if (pergunta === "não") {
+      alert("Obrigado pela preferência!");
+      mostraListaNaTela();
+      break;
+    }
+  }
+}
